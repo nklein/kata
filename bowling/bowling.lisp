847a6e76d161b46ae185574aff5c9a11d6ee1b13
Patrick Stein <pat@nklein.com>
2013-01-09 23:59:59 -0600

    Fifth attempt at bowling game kata in Common Lisp

--- bowling/bowling.lisp
+++ bowling/bowling.lisp
@@ -7,38 +7,49 @@
 
 (defgeneric roll (game pins)
   (:method ((game game) (pins integer))
-     (with-accessors ((index fill-pointer)) (rolls game)
-        (setf (aref (rolls game) index) pins)
-        (incf index))))
+    (let ((rolls (rolls game)))
+      (with-accessors ((index fill-pointer)) rolls
+        (setf (aref rolls index) pins)
+        (incf index)))))
 
-(defgeneric regular-score (game index)
+(defgeneric roll-many (game &key initial rolls pins)
+  (:method ((game game) &key initial rolls pins)
+     (mapc (lambda (pins) (roll game pins)) initial)
+     (dotimes (ii rolls (score game))
+       (roll game pins))))
+
+(defgeneric sum-of-next-two-balls (game index)
   (:method ((game game) (index integer))
-     (let ((rolls (rolls game)))
-       (+ (aref rolls index)
-          (aref rolls (1+ index))))))
+    (let ((rolls (rolls game)))
+      (+ (aref rolls index) (aref rolls (1+ index))))))
+
+(defgeneric regular-frame-score (game index)
+  (:method ((game game) (index integer))
+    (sum-of-next-two-balls game index)))
 
 (defgeneric strike-p (game index)
   (:method ((game game) (index integer))
      (= 10 (aref (rolls game) index))))
 
-(defgeneric strike-score (game index)
+(defgeneric strike-frame-score (game index)
   (:method ((game game) (index integer))
-     (+ 10 (regular-score game (1+ index)))))
+    (+ 10 (sum-of-next-two-balls game (1+ index)))))
 
 (defgeneric spare-p (game index)
   (:method ((game game) (index integer))
-     (= 10 (regular-score game index))))
+     (= 10 (regular-frame-score game index))))
 
-(defgeneric spare-score (game index)
+(defgeneric spare-frame-score (game index)
   (:method ((game game) (index integer))
-     (+ 10 (aref (rolls game) (+ index 2)))))
+    (let ((rolls (rolls game)))
+      (+ 10 (aref rolls (+ index 2))))))
 
 (defgeneric frame-score (game index)
   (:method ((game game) (index integer))
      (cond
-      ((strike-p game index) (values (strike-score game index) 1))
-      ((spare-p game index) (values (spare-score game index) 2))
-      (t (values (regular-score game index) 2)))))
+      ((strike-p game index) (values (strike-frame-score game index) 1))
+      ((spare-p game index) (values (spare-frame-score game index) 2))
+      (t (values (regular-frame-score game index) 2)))))
 
 (defgeneric score (game)
   (:method ((game game))
@@ -48,33 +59,25 @@
                       (incf index inc)
                       score))))
 
-(defgeneric roll-many (game &key initial rolls pins)
-  (:method ((game game) &key initial rolls pins)
-     (mapc (lambda (pins)
-             (roll game pins))
-           initial)
-     (dotimes (ii rolls (score game))
-       (roll game pins))))
-
 (ql:quickload :nst)
 
 (nst:def-fixtures game-instance ()
    (game))
 
 (nst:def-test-group bowling-game-kata-tests (game-instance)
-    (:each-setup (setf game (make-instance 'game)))
-  
-  (nst:def-test all-gutter-balls-test (:equal 0)
-    (roll-many game :rolls 20 :pins 0))
-  
-  (nst:def-test all-one-balls-test (:equal 20)
-    (roll-many game :rolls 20 :pins 1))
-  
-  (nst:def-test spare-test (:equal 14)
-    (roll-many game :initial '(5 5 1 2) :rolls 16 :pins 0))
-  
-  (nst:def-test strike-test (:equal 19)
-    (roll-many game :initial '(10 1 2 3 0) :rolls 14 :pins 0))
-  
-  (nst:def-test perfect-game-test (:equal 300)
-    (roll-many game :rolls 12 :pins 10)))
+           (:each-setup (setf game (make-instance 'game)))
+           
+   (nst:def-test all-gutter-balls-test (:equal 0)
+     (roll-many game :rolls 20 :pins 0))
+
+   (nst:def-test all-one-balls-test (:equal 20)
+     (roll-many game :rolls 20 :pins 1))
+   
+   (nst:def-test spare-test (:equal 14)
+     (roll-many game :initial '(5 5 1 2) :rolls 16 :pins 0))
+   
+   (nst:def-test strike-test (:equal 19)
+     (roll-many game :initial '(10 1 2 3 0) :rolls 14 :pins 0))
+   
+   (nst:def-test perfect-game-test (:equal 300)
+     (roll-many game :rolls 12 :pins 10)))
