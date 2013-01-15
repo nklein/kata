c14e97f1270bca9e6e281cfb192dc4f707c56e20
Patrick Stein <pat@nklein.com>
2013-01-14 23:59:58 -0600

    Eighth attempt at bowling game kata in Common Lisp

--- bowling/bowling.lisp
+++ bowling/bowling.lisp
@@ -1,80 +1,74 @@
-
 (defclass game ()
   ((rolls :initform (make-array '(21) :element-type 'integer
                                       :initial-element 0
                                       :fill-pointer 0)
-          :reader rolls)))
-
-(defgeneric sum-of-two-balls (game index)
-  (:method ((game game) (index integer))
-     (let ((rolls (rolls game)))
-       (+ (aref rolls index) (aref rolls (1+ index))))))
-
-(defgeneric strike-p (game index)
-  (:method ((game game) (index integer))
-     (let ((rolls (rolls game)))
-       (= 10 (aref rolls index)))))
-
-(defgeneric strike-score (game index)
-  (:method ((game game) (index integer))
-     (+ 10 (sum-of-two-balls game (1+ index)))))
-
-(defgeneric spare-p (game index)
-  (:method ((game game) (index integer))
-     (= 10 (sum-of-two-balls game index))))
-
-(defgeneric spare-score (game index)
-  (:method ((game game) (index integer))
-     (+ 10 (aref (rolls game) (+ index 2)))))
-
-(defgeneric regular-score (game index)
-  (:method ((game game) (index integer))
-     (sum-of-two-balls game index)))
-
-(defgeneric score (game)
-  (:method ((game game))
-     (loop :for frame :from 1 :to 10
-           :for ii :from 0 :by 2
-           :summing (cond
-                     ((strike-p game ii) (prog1
-                                             (strike-score game ii)
-                                           (decf ii)))
-                     ((spare-p game ii) (spare-score game ii))
-                     (t (regular-score game ii))))))
+          :accessor rolls)))
 
 (defgeneric roll (game pins)
   (:method ((game game) (pins integer))
-     (let ((rolls (rolls game)))
-       (with-accessors ((index fill-pointer)) rolls
-          (setf (aref rolls index) pins)
-          (incf index)))))
+    (let ((rolls (rolls game)))
+      (with-accessors ((index fill-pointer)) rolls
+        (setf (aref rolls index) pins)
+        (incf index)))))
+
+(defun two-ball-sum (game index)
+  (let ((rolls (rolls game)))
+    (+ (aref rolls index) (aref rolls (1+ index)))))
+
+(defun strike-p (game index)
+  (= 10 (aref (rolls game) index)))
+
+(defun strike-score (game index)
+  (+ 10 (two-ball-sum game (1+ index))))
+
+(defun spare-p (game index)
+  (= 10 (two-ball-sum game index)))
+
+(defun spare-score (game index)
+  (+ 10 (aref (rolls game) (+ index 2))))
+
+(defun regular-score (game index)
+  (+ (two-ball-sum game index)))
+
+(defgeneric score (game)
+  (:method ((game game))
+    (loop :for ii :from 0 :to (fill-pointer (rolls game))
+          :for frame :from 1 :to 10
+          :summing (cond
+                    ((strike-p game ii) (strike-score game ii))
+                    ((spare-p game ii) (prog1
+                                           (spare-score game ii)
+                                         (incf ii)))
+                    (t (prog1
+                           (regular-score game ii)
+                         (incf ii)))))))
 
 (defgeneric roll-many (game &key initial rolls pins)
   (:method ((game game) &key initial rolls pins)
-     (dolist (pins initial)
-       (roll game pins))
-     (dotimes (ii rolls (score game))
-       (roll game pins))))
+    (dolist (pins initial)
+      (roll game pins))
+    (dotimes (ii rolls (score game))
+      (roll game pins))))
 
 (ql:quickload :nst)
 
 (nst:def-fixtures game-instance ()
-   (game))
+  (game))
 
 (nst:def-test-group bowling-game-kata-tests (game-instance)
-          (:each-setup (setf game (make-instance 'game)))
-          
-   (nst:def-test all-gutter-balls-test (:equal 0)
-      (roll-many game :rolls 20 :pins 0))
-   
-   (nst:def-test all-one-balls-test (:equal 20)
-      (roll-many game :rolls 20 :pins 1))
-   
-   (nst:def-test spare-test (:equal 14)
-      (roll-many game :initial '(5 5 1 2) :rolls 16 :pins 0))
-   
-   (nst:def-test strike-test (:equal 19)
-      (roll-many game :initial '(10 1 2 3 0) :rolls 14 :pins 0))
-   
-   (nst:def-test perfect-game-test (:equal 300)
-      (roll-many game :rolls 12 :pins 10)))
+        (:each-setup (setf game (make-instance 'game)))
+        
+  (nst:def-test all-gutter-balls-test (:equal 0)
+    (roll-many game :rolls 20 :pins 0))
+  
+  (nst:def-test all-one-balls-test (:equal 20)
+    (roll-many game :rolls 20 :pins 1))
+  
+  (nst:def-test spare-test (:equal 14)
+    (roll-many game :initial '(5 5 1 2) :rolls 16 :pins 0))
+  
+  (nst:def-test strike-test (:equal 19)
+    (roll-many game :initial '(10 1 2 3 0) :rolls 14 :pins 0))
+  
+  (nst:def-test perfect-game-test (:equal 300)
+    (roll-many game :rolls 12 :pins 10)))
