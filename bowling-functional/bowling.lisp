434fa6055e01b4609dc8bfb32a17c2817a01dcdd
Patrick Stein <pat@nklein.com>
2013-01-20 21:16:35 -0600

    Third attempt at bowling game kata in functional-style Common Lisp (59 SLOC)

--- bowling-functional/bowling.lisp
+++ bowling-functional/bowling.lisp
@@ -1,10 +1,12 @@
+;;; http://butunclebob.com/ArticleS.UncleBob.TheBowlingGameKata
 
 (defclass game ()
-  ((rolls :initarg :rolls :initform nil :reader rolls)))
+  ((rolls :initarg :rolls :reader rolls))
+  (:default-initargs :rolls nil))
 
 (defgeneric roll (game pins)
   (:method ((game game) (pins integer))
-    (make-instance 'game :rolls (list* pins (rolls game)))))
+     (make-instance 'game :rolls (list* pins (rolls game)))))
 
 (defun two-ball-sum (rolls)
   (+ (first rolls) (second rolls)))
@@ -24,45 +26,53 @@
 (defun regular-score (rolls)
   (two-ball-sum rolls))
 
-(defun calculate-score (rolls frame)
-  (flet ((add-to-score (score next)
-           (+ score (calculate-score next (1+ frame)))))
+(defun score-rolls (rolls frame)
+  (flet ((add (amount rest)
+           (+ amount (score-rolls rest (1+ frame)))))
     (cond
      ((null rolls) 0)
      ((< 10 frame) 0)
-     ((strike-p rolls) (add-to-score (strike-score rolls) (cdr rolls)))
-     ((spare-p rolls)  (add-to-score (spare-score rolls) (cddr rolls)))
-     (t                (add-to-score (regular-score rolls) (cddr rolls))))))
+     ((strike-p rolls) (add (strike-score rolls) (cdr rolls)))
+     ((spare-p rolls) (add (spare-score rolls) (cddr rolls)))
+     (t (add (regular-score rolls) (cddr rolls))))))
 
 (defgeneric score (game)
   (:method ((game game))
-    (calculate-score (reverse (rolls game)) 1)))
+     (score-rolls (reverse (rolls game)) 1)))
 
-(defun roll-many (game &key initial rolls pins)
-  (cond
-   (initial (roll-many (roll game (first initial))
-                       :initial (rest initial)
-                       :rolls rolls
-                       :pins pins))
-   ((zerop rolls) game)
-   (t (roll-many (roll game pins) :rolls (1- rolls) :pins pins))))
+(defgeneric roll-many (game &key initial rolls pins)
+  (:method ((game game) &key initial rolls pins)
+    (cond
+     (initial (roll-many (roll game (first initial))
+                         :initial (rest initial)
+                         :rolls rolls
+                         :pins pins))
+     ((zerop rolls) game)
+     (t (roll-many (roll game pins)
+                   :rolls (1- rolls)
+                   :pins pins)))))
 
 (ql:quickload :nst)
 
+(nst:def-criterion (:score (target) (game))
+  (let ((actual (score game)))
+    (if (= actual target)
+        (nst:make-success-report)
+      (nst:make-failure-report :format "Expected score ~A but got ~A"
+                               :args (list target actual)))))
+
 (nst:def-test-group bowling-game-kata-tests ()
-  (nst:def-test all-gutter-balls (:equal 0)
-    (score (roll-many (make-instance 'game) :rolls 20 :pins 0)))
+  (nst:def-test all-gutter-balls-test (:score 0)
+    (roll-many (make-instance 'game) :rolls 20 :pins 0))
   
-  (nst:def-test all-one-balls (:equal 20)
-    (score (roll-many (make-instance 'game) :rolls 20 :pins 1)))
+  (nst:def-test all-one-balls-test (:score 20)
+    (roll-many (make-instance 'game) :rolls 20 :pins 1))
+
+  (nst:def-test spare-test (:score 14)
+    (roll-many (make-instance 'game) :initial '(5 5 1 2) :rolls 16 :pins 0))
   
-  (nst:def-test spare-test (:equal 14)
-    (score (roll-many (make-instance 'game)
-                      :initial '(5 5 1 2) :rolls 16 :pins 0)))
+  (nst:def-test strike-test (:score 19)
+    (roll-many (make-instance 'game) :initial '(10 1 2 3 0) :rolls 14 :pins 0))
   
-  (nst:def-test strike-test (:equal 19)
-    (score (roll-many (make-instance 'game)
-                      :initial '(10 1 2 3 0) :rolls 14 :pins 0)))
-  
-  (nst:def-test perfect-game-test (:equal 300)
-    (score (roll-many (make-instance 'game) :rolls 12 :pins 10))))
+  (nst:def-test perfect-game-test (:score 300)
+    (roll-many (make-instance 'game) :rolls 12 :pins 10)))
