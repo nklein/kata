8d1905ba1bcbfb03fbc706a4eda078eb5b041e65
Patrick Stein <pat@nklein.com>
2013-02-15 17:42:02 -0600

    Sixth attempt at bowling game kata in functional-style Common Lisp (61 SLOC)

--- bowling-functional/bowling.lisp
+++ bowling-functional/bowling.lisp
@@ -1,8 +1,17 @@
 ;;; http://butunclebob.com/ArticleS.UncleBob.TheBowlingGameKata
 
+;; Start: Fri Feb 15 17:09:15 CST 2013
+;; End:   Fri Feb 15 17:38:13 CST 2013
+;; SLOC:  61 bowling.lisp
+
 (defclass game ()
   ((rolls :initarg :rolls :reader rolls))
   (:default-initargs :rolls nil))
+
+(defgeneric roll (game pins)
+  (:method ((game game) (pins integer))
+     (make-instance 'game :rolls (append (rolls game)
+                                         (list pins)))))
 
 (defun two-ball-sum (rolls)
   (+ (first rolls) (second rolls)))
@@ -22,41 +31,35 @@
 (defun regular-score (rolls)
   (two-ball-sum rolls))
 
-(defun score-rolls (rolls score frame)
-  (flet ((add (n &optional (next (cddr rolls)))
-           (score-rolls next (+ n score) (1+ frame))))
+(defun score-rolls (rolls frame)
+  (flet ((accumulate (score-fn &optional (next-fn #'cddr))
+           (+ (funcall score-fn rolls)
+              (score-rolls (funcall next-fn rolls)
+                           (1+ frame)))))
     (cond
-      ((null rolls) score)
-      ((< 10 frame) score)
-      ((strike-p rolls) (add (strike-score rolls) (rest rolls)))
-      ((spare-p rolls) (add (spare-score rolls)))
-      (t (add (regular-score rolls))))))
+     ((null rolls)     0)
+     ((< 10 frame)     0)
+     ((strike-p rolls) (accumulate #'strike-score #'cdr))
+     ((spare-p rolls)  (accumulate #'spare-score))
+     (t                (accumulate #'regular-score)))))
 
 (defgeneric score (game)
   (:method ((game game))
-    (score-rolls (rolls game) 0 1)))
+     (score-rolls (rolls game) 1)))
 
-(defgeneric roll (game pins)
-  (:method ((game game) (pins integer))
-           (make-instance 'game :rolls (append (rolls game) (list pins)))))
-
-(defun roll-start (game start)
-  (if start
-      (roll-start (roll game (first start)) (rest start))
+(defun roll-constantly (game balls pins)
+  (if (plusp balls)
+      (roll-constantly (roll game pins) (1- balls) pins)
     game))
 
-(defun roll-constantly (game n pins)
-  (if (plusp n)
-      (roll-constantly (roll game pins) (1- n) pins)
+(defun roll-list (game list)
+  (if list
+      (roll-list (roll game (first list)) (rest list))
     game))
-
-(defgeneric roll-many (game &key start how-many pins)
-  (:method ((game game) &key start how-many pins)
-    (roll-constantly (roll-start game start) how-many pins)))
 
 (ql:quickload :nst)
 
-(nst:def-criterion (:score (target) (game))
+(nst:def-criterion (:bowling-score (target) (game))
   (let ((actual (score game)))
     (if (= actual target)
         (nst:make-success-report)
@@ -64,19 +67,19 @@
                                :args (list target actual)))))
 
 (nst:def-test-group bowling-game-kata-tests ()
-  (nst:def-test all-gutter-balls-test (:score 0)
-    (roll-many (make-instance 'game) :how-many 20 :pins 0))
+  (nst:def-test all-gutter-balls-test (:bowling-score 0)
+    (roll-constantly (make-instance 'game) 20 0))
   
-  (nst:def-test all-one-balls-test (:score 20)
-    (roll-many (make-instance 'game) :how-many 20 :pins 1))
+  (nst:def-test all-one-balls-test (:bowling-score 20)
+    (roll-constantly (make-instance 'game) 20 1))
   
-  (nst:def-test spare-test (:score 14)
-    (roll-many (make-instance 'game)
-               :start '(5 5 1 2) :how-many 16 :pins 0))
+  (nst:def-test spare-test (:bowling-score 14)
+    (roll-constantly (roll-list (make-instance 'game) '(5 5 1 2))
+                     16 0))
   
-  (nst:def-test strike-test (:score 19)
-    (roll-many (make-instance 'game)
-               :start '(10 1 2 3 0) :how-many 14 :pins 0))
-
-  (nst:def-test perfect-game (:score 300)
-    (roll-many (make-instance 'game) :how-many 12 :pins 10)))
+  (nst:def-test strike-test (:bowling-score 19)
+    (roll-constantly (roll-list (make-instance 'game) '(10 1 2 3 0))
+                     14 0))
+  
+  (nst:def-test perfect-game-test (:bowling-score 300)
+    (roll-constantly (make-instance 'game) 12 10)))
