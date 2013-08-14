d552a2d14fff5700abaf14b78aa9356ad1e3a13e
Patrick Stein <pat@nklein.com>
2013-08-14 11:58:00 -0500

    Thirteenth attempt at bowling game kata in functional-style Common Lisp (50 SLOC)

--- bowling-functional/bowling.lisp
+++ bowling-functional/bowling.lisp
@@ -1,76 +1,72 @@
 ;;; http://butunclebob.com/ArticleS.UncleBob.TheBowlingGameKata
 
-;;; Start: Tue Jul 30 14:52:49 CDT 2013
-;;; End:   Tue Jul 30 15:28:28 CDT 2013
-;;; SLOC:  53
+;;; Start: Wed Aug 14 11:24:35 CDT 2013
+;;; End: Wed Aug 14 11:56:07 CDT 2013
+;;; SLOC: 50
 
 (defclass game ()
-  ((rolls :initarg :rolls :reader rolls :type list))
+  ((rolls :initarg :rolls :reader rolls))
   (:default-initargs :rolls nil))
 
-(defgeneric roll (game pins)
-  (:method ((game game) pins)
-     (make-instance 'game :rolls (append (rolls game) (list pins)))))
+(defmethod roll ((game game) pins)
+  (make-instance 'game :rolls (append (rolls game) (list pins))))
 
 (defun two-ball-sum (rolls)
   (+ (first rolls) (second rolls)))
 
-(defun frame-score (rolls)
-  (two-ball-sum rolls))
-
-(defun strike? (rolls)
+(defun strike-p (rolls)
   (= (first rolls) 10))
 
 (defun strike-score (rolls)
   (+ 10 (two-ball-sum (rest rolls))))
 
-(defun spare? (rolls)
+(defun spare-p (rolls)
   (= (two-ball-sum rolls) 10))
 
 (defun spare-score (rolls)
   (+ 10 (third rolls)))
 
-(defun add-score (rolls score frame)
-  (flet ((add (amount &optional (next #'cddr))
-              (add-score (funcall next rolls) (+ amount score) (1+ frame))))
+(defun score-rolls (rolls score frame)
+  (flet ((add (points &optional (next-fn #'cddr))
+           (score-rolls (funcall next-fn rolls) (+ score points) (1+ frame))))
     (cond
-     ((< 10 frame)    score)
-     ((strike? rolls) (add (strike-score rolls) #'cdr))
-     ((spare? rolls)  (add (spare-score rolls)))
-     (t               (add (frame-score rolls))))))
+      ((or (null rolls) (< 10 frame)) score)
+      ((strike-p rolls) (add (strike-score rolls) #'cdr))
+      ((spare-p rolls) (add (spare-score rolls)))
+      (t (add (two-ball-sum rolls))))))
 
-(defgeneric score (game)
-  (:method ((game game))
-     (add-score (rolls game) 0 1)))
-
-(defun roll-many (game &key initial pins rolls)
-  (cond
-   ((zerop rolls) game)
-   (initial (roll-many (roll game (first initial))
-                       :initial (rest initial) :pins pins :rolls rolls))
-   (t (roll-many (roll game pins) :pins pins :rolls (1- rolls)))))
+(defmethod score ((game game))
+  (score-rolls (rolls game) 0 1))
 
 (ql:quickload :nst)
 
+(defun roll-many (game &key initial pins rolls)
+  (cond
+    (initial (roll-many (roll game (first initial))
+                        :initial (rest initial) :pins pins :rolls rolls))
+    ((zerop rolls) game)
+    (t (roll-many (roll game pins) :pins pins :rolls (1- rolls)))))
+
 (nst:def-criterion (:bowling-score (target) (&rest roll-options))
-  (let ((got (score (apply #'roll-many (make-instance 'game) roll-options))))
-    (if (= target got)
+  (let* ((game (apply #'roll-many (make-instance 'game) roll-options))
+         (got (score game)))
+    (if (= got target)
         (nst:make-success-report)
-      (nst:make-failure-report :format "Expected score of ~D but got ~A"
-                               :args (list target got)))))
+        (nst:make-failure-report :format "Expected ~D but got ~A~%"
+                                 :args (list target got)))))
 
-(nst:def-test-group bowling-game-tests ()
-  (nst:def-test zero-game (:bowling-score 0)
-    :pins 0 :rolls 20)
+(nst:def-test-group bowling-game-kata-tests ()
+  (nst:def-test gutter-game-test (:bowling-score 0)
+    :rolls 20 :pins 0)
 
-  (nst:def-test ones-game (:bowling-score 20)
-    :pins 1 :rolls 20)
+  (nst:def-test ones-game-test (:bowling-score 20)
+    :rolls 20 :pins 1)
 
-  (nst:def-test spare-game (:bowling-score 15)
-    :initial '(5 5 2 1) :pins 0 :rolls 16)
+  (nst:def-test spare-test (:bowling-score 14)
+    :initial '(5 5 1 2) :rolls 16 :pins 0)
 
-  (nst:def-test strike-game (:bowling-score 21)
-    :initial '(10 3 2 1 0) :pins 0 :rolls 14)
+  (nst:def-test strike-test (:bowling-score 19)
+    :initial '(10 1 2 3 0) :rolls 16 :pins 0)
 
-  (nst:def-test perfect-game (:bowling-score 300)
-    :pins 10 :rolls 12))
+  (nst:def-test perfect-game-test (:bowling-score 300)
+    :rolls 12 :pins 10))
